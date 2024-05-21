6642ac83c45a57f526b6f56294569c7e6bea9e10
youngtaek_hong <youngtaek_hong@dweax.com>
2024-05-21 17:29:53 +0900

    Feat : 홈 화면 작업 및 리다이렉션 기능 추가

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -1,9 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
-import { TestPage } from "@pages/TestPage";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import { Home } from "@pages/Home";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <TestPage />,
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <Navigate replace to="/home" />,
   },
 ]);
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Canvas } from "@widgets/Canvas";
+
+export const Home: React.FC = () => {
+  return <Canvas />;
+};
